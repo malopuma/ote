acb792d45d5b05f36f6912291c949543e379e4d7
beeni <benjaminwaldmann@proton.me>
2026-02-08 08:15:30 +0100

    pygame tutorial pt 1 - first commit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print ("Hello crazy cloud-git-world")
+# on th edges - entry point
+# building game centered on calm gameplay in a stunning, evolving landscape
+
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((516,516))
+pygame.display.set_caption("On The Edges")
+
+x [int] = 32
+y [int] = 32
+width [int] = 16
+height [int] = 16
+velocity [int] = 10
+
+run [bool] = True
+
+while run == True:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
