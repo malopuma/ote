0b674e6f2356475f2944bf6990c04344cb2db712
beeni <benjaminwaldmann@proton.me>
2026-02-08 08:47:27 +0100

    initial game loop corrected

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ pygame.init()
 window = pygame.display.set_mode((516,516))
 pygame.display.set_caption("On The Edges")
 
-x [int] = 32
-y [int] = 32
-width [int] = 16
-height [int] = 16
-velocity [int] = 10
+# variables
+x: int = 32
+y: int = 32
+width: int = 16
+height: int = 16
+velocity: int = 10
 
-run [bool] = True
+run: bool = True
 
+# game loop
 while run == True:
     pygame.time.delay(100)
 
