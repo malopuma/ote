91a64fe90827087044b3f1126748685903b53974
beeni <benjaminwaldmann@proton.me>
2026-02-08 09:12:32 +0100

    basic movement and charakter

--- main.py
+++ main.py
@@ -25,5 +25,20 @@ while run == True:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= velocity
+    if keys[pygame.K_RIGHT]:
+        x += velocity
+    if keys[pygame.K_UP]:
+        y -= velocity
+    if keys[pygame.K_DOWN]:
+        y += velocity
+
+    window.fill((0, 0, 0))
+    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
 pygame.quit()
 
